3cf8d5c3955964c782be10d96b162374c3bb3922
Claudia Bañuelos <claneb15@gmail.com>
2018-11-02 10:17:38 -0700

    se cambio el texto en el usuario

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+//var miTitulo = document.querySelector('h1');
+//miTitulo.innerHTML = "Hello world!";
+
+document.querySelector('html').onclick = function() {
+    //alert('Ouch! Deja de pincharme!');
+}
+
+var miBoton = document.querySelector('button');
+var miTitulo = document.querySelector( 'h1');
+
+if (!localStorage.getItem('nombre')) {
+    		estableceNombreUsuario();
+		}
+		else {
+		    var nombreAlmacenado = localStorage.getItem('nombre');
+		    miTitulo.textContent = 'Mozilla es fresco,' + nombreAlmacenado;
+		}
+
+function estableceNombreUsuario() {
+    var miNombre = prompt('Por favor, ingresa tu nombre.');
+    localStorage.setItem('nombre', miNombre);
+    miTitulo.textContent = 'Mozilla es fresco,' + miNombre;
+}
+
+
+miBoton.onclick = function() {
+    estableceNombreUsuario();
+}
